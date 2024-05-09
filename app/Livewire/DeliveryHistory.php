dcd24bff65aa39fe7b475f1d1e5160467f8a0c89
kelvi-zignuys <kelvib@zignuts.com>
2024-05-09 15:22:29 +0530

    reverb and jetstream packges implementation

---
+++ app/Livewire/DeliveryHistory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire;
+
+use Carbon\Carbon;
+use Livewire\Component;
+use App\Events\PackageSent;
+use Livewire\Attributes\On;
+
+class DeliveryHistory extends Component
+{
+    public array $packageStatuses = [
+    ];
+
+    public string $status = '';
+
+    public function submitStatus()
+    {
+        PackageSent::dispatch(auth()->user()->name, $this->status, Carbon::now());
+
+        $this->reset('status');
+    }
+
+    #[On('echo:delivery,PackageSent')]
+    public function onPackageSent($event)
+    {
+        $this->packageStatuses[] = $event;
+    }
+
+    public function render()
+    {
+        return view('livewire.delivery-history');
+    }
+
+}
